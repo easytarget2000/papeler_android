c97e4a4adbe73fe609e0858154619cbca2f2bc56
Michel Sievers <michel@easy-target.org>
2017-01-24 00:09:42 +0100

    Initial commit after mv

---
+++ app/src/main/java/org/eztarget/papeler/MyPoint.java
@@ -0,0 +1,17 @@
+package org.eztarget.papeler;
+
+/**
+ * Created by michelsievers on 23/01/2017.
+ */
+
+public class MyPoint {
+    String text;
+    float x;
+    float y;
+
+    public MyPoint(String text, float x, float y) {
+        this.text = text;
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ app/src/main/java/org/eztarget/papeler/SetWallpaperActivity.java
@@ -0,0 +1,29 @@
+package org.eztarget.papeler;
+
+import android.app.Activity;
+import android.app.WallpaperManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by michelsievers on 23/01/2017.
+ */
+
+public class SetWallpaperActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(
+                WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
+        intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+                new ComponentName(this, WallpapelerService.class));
+        startActivity(intent);
+    }
+}
