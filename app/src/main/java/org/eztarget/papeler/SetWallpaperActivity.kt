510460f12fed6f1df0f78a16a931a516f4f13f0b
Michel Sievers <michel@easy-target.eu>
2019-07-26 15:35:59 +0200

    Converted SetWallpaperActivity.

---
+++ app/src/main/java/org/eztarget/papeler/SetWallpaperActivity.kt
@@ -0,0 +1,48 @@
+package org.eztarget.papeler
+
+import android.app.Activity
+import android.app.WallpaperManager
+import android.content.ActivityNotFoundException
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+
+class SetWallpaperActivity : Activity() {
+
+    private var didSetWallpaper = false
+    var previewWallpaperIntent = Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER)
+    var selectWallpaperIntent = Intent(WallpaperManager.ACTION_LIVE_WALLPAPER_CHOOSER)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (didSetWallpaper) {
+            finish()
+        } else {
+            setWallpaper()
+        }
+    }
+
+    private fun setWallpaper() {
+        previewWallpaperIntent.putExtra(
+                WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+                ComponentName(this, WayprService::class.java)
+        )
+
+        try {
+            startActivity(previewWallpaperIntent)
+        } catch (e: ActivityNotFoundException) {
+            startActivity(selectWallpaperIntent)
+        }
+
+        didSetWallpaper = true
+    }
+
+    fun onClick(view: View) {}
+}
